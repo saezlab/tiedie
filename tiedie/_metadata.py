509d70c9fb4aeeb835b1b573917095276aff594b
deeenes <turei.denes@gmail.com>
2026-01-21 17:31:18 +0100

    Bump version: 2.0.0 → 2.0.1

--- tiedie/_metadata.py
+++ tiedie/_metadata.py
@@ -4,7 +4,7 @@ __all__ = ['__version__', '__author__', '__license__']
 
 import importlib.metadata
 
-_FALLBACK_VERSION = '2.0.0'
+_FALLBACK_VERSION = '2.0.1'
 
 try:
     __version__ = importlib.metadata.version('tiedie')
